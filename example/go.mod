44d36ff69faf61ed25147d4cd020940ec947f52a
Daniel Nekrasov <nd@rktv.ru>
2021-07-13 18:17:23 +0500

    docs: add redis example

--- example/go.mod
+++ example/go.mod
@@ -4,4 +4,8 @@ go 1.16
 
 replace gitlab.loc/sdk-login/login-sdk-go => ../
 
-require gitlab.loc/sdk-login/login-sdk-go v0.0.0-00010101000000-000000000000
+require (
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/onsi/gomega v1.14.0 // indirect
+	gitlab.loc/sdk-login/login-sdk-go v0.0.0-00010101000000-000000000000
+)
--- example/main.go
+++ example/main.go
@@ -1,14 +1,58 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/go-redis/redis"
 	"gitlab.loc/sdk-login/login-sdk-go"
+	"gitlab.loc/sdk-login/login-sdk-go/model"
 	"log"
+	"time"
 )
 
-func main() {
-	config := login_sdk_go.Config{
-		ShaSecretKey: "test",
+type RedisCache struct {
+	client *redis.Client
+	s      *login_sdk_go.ProjectKeysGetter
+}
+
+func NewRedisCache(client *redis.Client) *RedisCache {
+	return &RedisCache{
+		client: client,
 	}
+}
+
+func (r RedisCache) Get(projectId string) (interface{}, bool) {
+	if r.client == nil {
+		return "", false
+	}
+	result, err := r.client.Get(projectId).Bytes()
+	var res []model.ProjectPublicKey
+	err = json.Unmarshal(result, &res)
+
+	return res, err == nil
+}
+
+func (r RedisCache) Set(projectId string, value interface{}) {
+	b, _ := json.Marshal(value)
+	r.client.Set(projectId, b, time.Hour)
+}
+
+func main() {
+	redisDSN := fmt.Sprintf("%s:%d", "127.0.0.1", 6379)
+
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:        redisDSN,
+		ReadTimeout: 1 * time.Second,
+		PoolSize:    20,
+	})
+
+	redisCache := NewRedisCache(redisClient)
+	config := login_sdk_go.Config{
+		ShaSecretKey:   "test",
+		LoginProjectId: "login-id",
+		Cache:          redisCache,
+	}
+
 	loginSdk, _ := login_sdk_go.New(config)
 	err := loginSdk.Validate("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.5mhBHqs5_DTLdINd9p5m7ZJ6XD0Xc55kIaCRY5r6HRA")
 	if !err.Valid() {
