293b34788672fcba6d591b525f8c7cb4c8b2e30a
Maksim Syrchikov <m.syrchikov@xsolla.com>
2024-05-27 15:13:57 +0800

    update default timeouts

--- internal/adapter/login/transport.go
+++ internal/adapter/login/transport.go
@@ -14,13 +14,13 @@ func NewDefaultTransport(ignoreSslErrors bool) *http.Transport {
 			InsecureSkipVerify: ignoreSslErrors,
 		},
 		DialContext: (&net.Dialer{
-			Timeout:   30 * time.Second,
-			KeepAlive: 30 * time.Second,
+			Timeout:   10 * time.Second,
+			KeepAlive: 10 * time.Second,
 		}).DialContext,
 		ForceAttemptHTTP2:     true,
 		MaxIdleConns:          100,
-		IdleConnTimeout:       90 * time.Second,
-		TLSHandshakeTimeout:   10 * time.Second,
+		IdleConnTimeout:       10 * time.Second,
+		TLSHandshakeTimeout:   1 * time.Second,
 		ExpectContinueTimeout: 1 * time.Second,
 	}
 }
